e3b0f2cfecb08617665fdee59d9b750af2ad9db5
kunjal-tagline <kunjal.tagline@gmail.com>
2022-03-30 11:56:30 +0530

    Apply watchQuery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'graphql';
+export class AppComponent implements OnInit {
+  public rates: any = [];
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.watchQuery();
+  }
+
+  public watchQuery() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.rates = result?.data?.rates;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApolloModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+      {
+        provide: APOLLO_OPTIONS,
+        useFactory(httpLink: HttpLink) {
+          return {
+            cache: new InMemoryCache(),
+            link: httpLink.create({
+              uri: 'https://48p1r2roz4.sse.codesandbox.io'
+            }),
+          };
+        },
+        deps: [HttpLink],
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
