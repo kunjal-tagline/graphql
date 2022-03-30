bd477b51168aa74b98d5f18429e00ec1ffc9f8cc
kunjal-tagline <kunjal.tagline@gmail.com>
2022-03-30 17:06:14 +0530

    Mutations and queries done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 
+const CREATE_ALBUM = gql`
+  mutation {
+    createAlbum(input: { userId: "2", title: "test" }) {
+      user {
+        id
+        email
+        website
+      }
+    }
+  }
+`;
+
+const UPDATE_ALBUM = gql`
+  mutation {
+    updateAlbum(id: "1", input: { title: "test", userId: "3" }) {
+      id
+      title
+      user {
+        username
+        email
+      }
+      photos {
+        data {
+          title
+        }
+      }
+    }
+  }
+`;
+
+const DELETE_POST = gql`
+  mutation {
+    deletePost(id: "3")
+  }
+`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public rates: any = [];
-  loading = true;
-  error: any;
-
+  public albumData: any;
+  public createAlbum: any = [];
+  public updateAlbum: any = [];
+  public photos: any;
   constructor(private apollo: Apollo) {}
 
   ngOnInit() {
     this.watchQuery();
+    this.createMutation();
+    this.updateMutation();
+    this.deletePost();
   }
 
   public watchQuery() {
@@ -22,17 +60,44 @@ export class AppComponent implements OnInit {
       .watchQuery({
         query: gql`
           {
-            rates(currency: "USD") {
-              currency
-              rate
+            album(id: "4") {
+              id
+              title
             }
           }
         `,
       })
       .valueChanges.subscribe((result: any) => {
-        this.rates = result?.data?.rates;
-        this.loading = result.loading;
-        this.error = result.error;
+        this.albumData = Object.values(result?.data?.album);
       });
+  }
+
+  public createMutation() {
+    this.apollo
+      .mutate({
+        mutation: CREATE_ALBUM,
+      })
+      .subscribe((res: any) => {
+        this.createAlbum = Object.values(res?.data?.createAlbum?.user);
+      });
+  }
+
+  public updateMutation() {
+    this.apollo
+      .mutate({
+        mutation: UPDATE_ALBUM,
+      })
+      .subscribe((res: any) => {
+        //console.log('res :>> ', res.data.updateAlbum);
+        this.updateAlbum = [res?.data?.updateAlbum];
+        this.photos = res.data.updateAlbum.photos.data;
+        //console.log('this.updateAlbum :>> ', res.data.updateAlbum.photos.data);
+      });
+  }
+
+  public deletePost() {
+    this.apollo.mutate({ mutation: DELETE_POST }).subscribe((res: any) => {
+      console.log('res :>> ', res?.data.deletePost);
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloModule, APOLLO_FLAGS, APOLLO_OPTIONS} from 'apollo-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
@@ -18,13 +18,19 @@ import { HttpClientModule} from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
+    {
+      provide: APOLLO_FLAGS,
+      useValue: {
+        useMutationLoading: true,
+      },
+    },
       {
         provide: APOLLO_OPTIONS,
         useFactory(httpLink: HttpLink) {
           return {
             cache: new InMemoryCache(),
             link: httpLink.create({
-              uri: 'https://48p1r2roz4.sse.codesandbox.io'
+              uri: 'https://graphqlzero.almansi.me/api'
             }),
           };
         },
