e61e9cd87da3397eb1cb8634730a86a3ac48c7a4
kunjal-tagline <kunjal.tagline@gmail.com>
2022-07-01 15:16:20 +0530

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { AddEditComponent } from './posts/add-edit/add-edit.component';
+import { SiglePostComponent } from './posts/sigle-post/sigle-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'posts',
+    component: PostListComponent,
+  },
+  {
+    path: 'posts/add',
+    component: AddEditComponent,
+  },
+  {
+    path: 'posts/edit/:id',
+    component: AddEditComponent,
+  },
+  {
+    path: 'posts/details/:id',
+    component: SiglePostComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
