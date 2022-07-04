fdc741320c30c2274dd973c9e5e0d3ee973cb12e
kunjal-tagline <kunjal.tagline@gmail.com>
2022-07-04 11:30:56 +0530

    Create service for ngrx data extending

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postService:PostService) { }
 
   ngOnInit(): void {
+    this.postService.getAll();
   }
-
 }
