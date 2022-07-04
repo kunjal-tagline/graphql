196f3e0ba2238b263df0532c87efc1281a1332d5
kunjal-tagline <kunjal.tagline@gmail.com>
2022-07-04 11:30:22 +0530

    Install data and effects, entity, store, store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AddEditComponent } from './posts/add-edit/add-edit.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { SiglePostComponent } from './posts/sigle-post/sigle-post.component';
 import { HomeComponent } from './home/home.component';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +25,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    EntityDataModule.forRoot(entityConfig),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
